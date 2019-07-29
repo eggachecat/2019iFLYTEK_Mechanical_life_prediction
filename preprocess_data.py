3db02378cfc8a0278059c7ad170364917a33f902
just-passerby <35185525+just-passerby@users.noreply.github.com>
2019-07-29 22:53:00 +0800

    Add files via upload

--- preprocess_data.py
+++ preprocess_data.py
@@ -18,7 +18,7 @@ def get_filelist (dir,flielist):
             get_filelist(new_dir,flielist)
     return flielist
 
-#处理数据
+#处理数据,添加特征
 def preprocess (data,df,name):
 
     #根据样本选择或处理特征
@@ -81,8 +81,8 @@ def preprocess (data,df,name):
     #压力信号2主要分布在一段区间上，剩余值较小，处理同上
     elif name == '压力信号2':
         length = len(data)
-        low_pressure = list(num for num in data if 0 <= num <=100)
-        high_pressure = list(num for num in data if 150 <= num)
+        low_pressure = list(num for num in data if 0 <= num <=50)
+        high_pressure = list(num for num in data if 200 <= num)
         low_percentage = np.sum(low_pressure) / length
         high_percentage = np.sum(high_pressure) / length
         df[name + '信号2低压力段标准差'] = np.std(low_pressure) * low_percentage
@@ -92,34 +92,38 @@ def preprocess (data,df,name):
     elif name == '转速信号1':
         length = len(data)
         low_pressure = list(num for num in data if 0 <= num <=100)
-        high_pressure = list(num for num in data if 150 <= num)
+        high_pressure = list(num for num in data if 3000 <= num)
         low_percentage = np.sum(low_pressure) / length
         high_percentage = np.sum(high_pressure) / length
+        df[name + '信号1低转速段均值'] = np.mean(low_pressure) * low_percentage
+        df[name + '信号1高转速段均值'] = np.mean(high_pressure) * high_percentage
         df[name + '信号1低转速段标准差'] = np.std(low_pressure) * low_percentage
         df[name + '信号1高转速段标准差'] = np.std(high_pressure) * high_percentage
     
     #同压力信号2
     elif name == '转速信号2':
         length = len(data)
-        low_pressure = list(num for num in data if 0 <= num <=100)
-        high_pressure = list(num for num in data if 150 <= num)
+        low_pressure = list(num for num in data if 0 <= num <=1000)
+        high_pressure = list(num for num in data if 10000 <= num)
         low_percentage = np.sum(low_pressure) / length
         high_percentage = np.sum(high_pressure) / length
-        df[name + '信号2低转速段标准差'] = np.std(low_pressure) * low_percentage
+        df[name + '信号2极低转速段均值'] = np.mean(low_pressure) * low_percentage
+        df[name + '信号2高转速段均值'] = np.mean(high_pressure) * high_percentage
+        df[name + '信号2极低转速段标准差'] = np.std(low_pressure) * low_percentage
         df[name + '信号2高转速段标准差'] = np.std(high_pressure) * high_percentage
 
     return df
 
 #处理单个训练样本
-def process_single_sample (e,train_percentage):
+def process_single_sample (path,train_percentage):
 
-    data = pd.read_csv(e)
+    data = pd.read_csv(path)
     #获取该零件寿命
     work_life = data['部件工作时长'].max()
     #获取在寿命一定百分比时间的数据
     data=data[data['部件工作时长']<=work_life*train_percentage]
     #创建数据集
-    dict_data = { 'train_file_name': os.path.basename(e) + str(train_percentage),
+    dict_data = { 'train_file_name': os.path.basename(path) + str(train_percentage),
                        'device': data['设备类型'][0],
                        'rest_life':work_life-data['部件工作时长'].max()
                      }
@@ -142,47 +146,68 @@ def process_single_sample (e,train_percentage):
     return features
 
 #整合处理训练集与测试集,并采用多线程
-def integrated_process (cpu,path_list,test_or_not,func):
+def integrated_process (path_list,test_or_not):
     
+    #测试集处理
     if test_or_not == True:
+        #测试集无需对数据进行分割处理
         train_percentage_list = [1]
-        apply_object = []
-        pool = Pool(cpu)
+        feature_list = []
         for path in path_list:
             for train_percentage in train_percentage_list:
-                apply_object.append(pool.apply_async(func, args=(path,train_percentage,)))
-        pool.close()
-        pool.join()
-        apply_object = [i.get() for i in apply_object]
-        features = apply_object[0]
-        for i in apply_object[1:]:
-            features = pd.concat([features, i], axis=0)
+                single_data = process_single_sample(path,train_percentage)
+                feature_list.append(single_data)
+        features = feature_list[0]
+        for item in feature_list:
+            features = pd.concat([features, item], axis=0)
         columns=features.columns.tolist()
-        for col in ['train_file_name','life']:
+        for col in ['train_file_name','rest_life']:
             columns.remove(col)
-        columns=['train_file_name']+columns+['life']
+        columns=['train_file_name']+columns+['rest_life']
         features['train_file_name']=features['train_file_name'].apply(lambda x:x[:-1])
         features=features.reindex(columns=columns)
 
-def asd (x):
-    x = x+1
-    return x
+    #训练集处理
+    if test_or_not == False:
+        #训练集目的为预测剩余寿命，故将数据集分割
+        train_percentage_list = [0.45,0.55,0.63,0.75,0.85]
+        feature_list = []
+        for path in path_list:
+            for train_percentage in train_percentage_list:
+                feature_list.append(process_single_sample(path,train_percentage))
+        features = feature_list[0]
+        for item in feature_list:
+            features = pd.concat([features, item], axis=0)
+        columns=features.columns.tolist()
+        for col in ['train_file_name','rest_life']:
+            columns.remove(col)
+        columns=['train_file_name']+columns+['rest_life']
+        features=features.reindex(columns=columns)
+    
+    return features
 
 #主进程，调试使用
 if __name__ == '__main__':
 
     #获取路径集
-    path_name= 'train'
-    train_list = get_filelist(path,[])
+    train_path = 'train'
+    test_path = 'test1'
+    train_list = get_filelist(train_path,[])
+    test_path = get_filelist(test_path,[])
 
-    #绘制图像观测数据分布特征
-    plt.figure()
-    for csv_path in train_list:
-        raw_data = pd.read_csv(csv_path)
-        plt.scatter(raw_data['部件工作时长'],raw_data['温度信号'])
+    # #绘制图像观测数据分布特征
+    # plt.figure()
+    # for csv_path in train_list:
+    #     raw_data = pd.read_csv(csv_path)
+    #     plt.scatter(raw_data['部件工作时长'],raw_data['压力信号2'])
 
-        #窗口最大化
-        mng = plt.get_current_fig_manager()
-        mng.window.state('zoomed') #works fine on Windows!
+    #     #窗口最大化
+    #     mng = plt.get_current_fig_manager()
+    #     mng.window.state('zoomed') #works fine on Windows!
 
-        plt.show()
+    #     plt.show()
+
+    # train=integrated_process(train_list,False)
+    # test =integrated_process(4,test_list,True,func)
+
+    # print (train)
